b8cf8785d5b1a186b76f8e04cc7676cde6aa89d0
Dilshod Akhmedov <akhmedov.20092004@gmail.com>
2024-04-17 10:41:13 +0500

    connected to mongodb database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    console.log("Database connection established")
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
